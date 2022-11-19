e82f8ac58c9901b74858fe5175e3268fa3b39e6f
Helton Barbosa <heltonbs@gmail.com>
2022-11-19 08:15:02 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,39 @@
+
+
+pokemon = {
+    'Pikachu': {
+        'status': ['HP: 300', 'Ataque: 510', 'Defesa: 400', 'Velocidade: 200', 'Total: 1.430'],
+        'habilidades': ['Choque do trovão', 'Cabeçada'],
+        'tipo': ['#001', 'grama', 'images/pikachu.png']
+    },
+
+    'Bulbasaur': {
+        'status': ['HP: 320', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        'habilidades': ['Choque do trovão', 'Cabeçada'],
+        'tipo': ['#025', 'elétrico', 'images/bulbasaur.png']
+    },
+
+    'Pikachu': {
+        'status': ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        'habilidades': ['Choque do trovão', 'Cabeçada'],
+        'tipo': ['#025', 'elétrico', 'images/pikachu.png']
+    },
+
+    'Pikachu': {
+        'status': ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        'habilidades': ['Choque do trovão', 'Cabeçada'],
+        'tipo': ['#025', 'elétrico', 'images/pikachu.png']
+    },
+
+    'Pikachu': {
+        'status': ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        'habilidades': ['Choque do trovão', 'Cabeçada'],
+        'tipo': ['#025', 'elétrico', 'images/pikachu.png']
+    },
+
+    'Pikachu': {
+        'status': ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        'habilidades': ['Choque do trovão', 'Cabeçada'],
+        'tipo': ['#025', 'elétrico', 'images/pikachu.png']
+    },
+}
